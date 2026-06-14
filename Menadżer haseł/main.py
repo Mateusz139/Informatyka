a9899e0f022ed1494d8092ef5b6fa65750cc829b
Mateusz139 <261762@edu.p.lodz.pl>
2026-06-14 15:20:20 +0200

    Delete Menadżer haseł directory

--- Menadżer haseł/main.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-from ui import PasswordManagerUI
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = PasswordManagerUI()
-    window.show()
-    sys.exit(app.exec())
